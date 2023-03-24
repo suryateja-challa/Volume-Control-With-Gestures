c87b313aca498181a76a906f66698b059b17a37b
suryateja-challa <challasuryateja29@gmail.com>
2023-03-24 23:49:50 +0530

    Changed file name. Module meant only for tracking hands.

--- HandTracker.py
+++ HandTracker.py
@@ -1,8 +1,13 @@
 import cv2
-import mediapipe as mp
 import time
+import numpy as np
+import mediapipe as mp
+
+wcam, hcam = 640, 480
 
 cap = cv2.VideoCapture(0)
+cap.set(3, wcam)
+cap.set(4, hcam)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
@@ -23,8 +28,8 @@ while True:
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
-                if id == 0:
+                #print(id, cx, cy)
+                if id == 4 or id == 8:
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
             mp.solutions.drawing_utils.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
@@ -32,7 +37,11 @@ while True:
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 25, 0), 3)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
